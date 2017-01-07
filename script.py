382c805992f1a73d25e7a81222a99e0b964d35a2
Konrad Kapron <Kapron.K4@gmail.com>
2017-01-07 21:55:00 +0100

    Python jest nieco bardziej czytelny, naprawilem tez chyba jednego buga

--- script.py
+++ script.py
@@ -29,41 +29,57 @@ def drawBoard(screen):
                 pygame.draw.circle(screen, colors.PAWN_BLACK, (int(x*Game.FIELD_SIZE+Game.FIELD_SIZE/2), int(y*Game.FIELD_SIZE+Game.FIELD_SIZE/2)), int(Game.FIELD_SIZE/2))
             elif Game.board.get(x,y) == LoA.Field.White:
                 pygame.draw.circle(screen, colors.PAWN_WHITE, (int(x*Game.FIELD_SIZE+Game.FIELD_SIZE/2), int(y*Game.FIELD_SIZE+Game.FIELD_SIZE/2)), int(Game.FIELD_SIZE/2))
+    pygame.display.update()
 
 def highlightField(screen, x, y, color):
     pygame.draw.rect(screen, color, (x, y, Game.FIELD_SIZE, Game.FIELD_SIZE), 4)
+    pygame.display.update()
 
-def pollEvents(screen):
+def highlight(screen):
+    highlightField(screen, Game.pawn_pos.x*Game.FIELD_SIZE, Game.pawn_pos.y*Game.FIELD_SIZE, colors.PAWN_HIGHLIGHT)
+    for position in Game.board.getAllPossibleMoves(Game.pawn_pos):
+        highlightField(screen, position.x*Game.FIELD_SIZE, position.y*Game.FIELD_SIZE, colors.FIELD_HIGHLIGHT)
+
+def resetCurrentPawn():
+    Game.pawn_pos = LoA.Position(-1, -1)
+
+def getPressedField(event):
+    return LoA.Position(int(event.pos[0]/Game.FIELD_SIZE), int(event.pos[1]/Game.FIELD_SIZE))
+
+def onPawnMove(target_pos):
+    Game.board = Game.board.getMoved(Game.pawn_pos, target_pos)
+    drawBoard(screen)
+
+    Game.board = LoA.AI_turn(Game.board)
+    drawBoard(screen)
+
+    pygame.event.clear()
+    resetCurrentPawn()
+
+def loopOnce(screen):
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             Game.running = False;
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                field_color = Game.board.get(int(event.pos[0]/Game.FIELD_SIZE), int(event.pos[1]/Game.FIELD_SIZE))
-                if field_color == Game.PLAYER:
-                    Game.pawn_pos = LoA.Position(int(event.pos[0]/Game.FIELD_SIZE), int(event.pos[1]/Game.FIELD_SIZE))
+                clicked_field_color = Game.board.get(getPressedField(event))
+
+                if clicked_field_color == Game.PLAYER: #choosing pawn
+                    Game.pawn_pos = getPressedField(event)
                     drawBoard(screen)
-                    highlightField(screen, Game.pawn_pos.x*Game.FIELD_SIZE, Game.pawn_pos.y*Game.FIELD_SIZE, colors.PAWN_HIGHLIGHT)
-                    for position in Game.board.getAllPossibleMoves(Game.pawn_pos):
-                        highlightField(screen, position.x*Game.FIELD_SIZE, position.y*Game.FIELD_SIZE, colors.FIELD_HIGHLIGHT)
-                    pygame.display.update()
-                else:
+                    highlight(screen)
+                elif Game.pawn_pos.x != -1:             #choosing where to move the pawn
                     possible_moves = list(Game.board.getAllPossibleMoves(Game.pawn_pos))
-                    target_pos = LoA.Position(int(event.pos[0]/Game.FIELD_SIZE), int(event.pos[1]/Game.FIELD_SIZE))
+                    target_pos = getPressedField(event)
                     for move in possible_moves:
                         if move.x == target_pos.x and move.y == target_pos.y:
-                            Game.board = Game.board.getMoved(Game.pawn_pos, target_pos)
-                            drawBoard(screen)
-                            pygame.display.update()
-                            Game.board = LoA.AI_turn(Game.board)
-                            drawBoard(screen)
-                            pygame.display.update()
-                            pygame.event.clear()
+                            onPawnMove(target_pos)
                             break
+
             elif event.button == 3:
+                resetCurrentPawn()
                 drawBoard(screen)
-                pygame.display.update()
                
 pygame.init()
 
@@ -72,7 +88,6 @@ screen.fill(colors.BACKGROUND)
 pygame.display.flip()
 
 drawBoard(screen)
-pygame.display.update()
 
 while Game.running:
-    pollEvents(screen)
+    loopOnce(screen)
